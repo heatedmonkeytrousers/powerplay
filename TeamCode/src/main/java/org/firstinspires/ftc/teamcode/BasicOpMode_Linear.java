372fde551ebc2625328a635a877b2d7836f27877
Mizukiee <codingforfun156@gmail.com>
2022-10-30 18:09:45 -0500

    Cleaned up processing code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Linear.java
@@ -62,6 +62,8 @@ public class BasicOpMode_Linear extends LinearOpMode {
     private DcMotor rearLeftDrive = null;
     private DcMotor rearRightDrive = null;
     private DcMotor elevatorDrive = null;
+    public static double MOTOR_PPR = 384.5;
+    private int totalCounts = 0;
 
     @Override
     public void runOpMode() {
@@ -71,11 +73,11 @@ public class BasicOpMode_Linear extends LinearOpMode {
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        frontLeftDrive  = hardwareMap.get(DcMotor.class, "frontLeftDrive");
-        frontRightDrive = hardwareMap.get(DcMotor.class, "frontRightDrive");
-        rearLeftDrive  = hardwareMap.get(DcMotor.class, "rearLeftDrive");
-        rearRightDrive = hardwareMap.get(DcMotor.class, "rearRightDrive");
-        elevatorDrive = hardwareMap.get(DcMotor.class, "elevatorDrive");
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "frontLeftDrive"); //ch3
+        frontRightDrive = hardwareMap.get(DcMotor.class, "frontRightDrive"); //ch2
+        rearLeftDrive = hardwareMap.get(DcMotor.class, "rearLeftDrive"); //ch1
+        rearRightDrive = hardwareMap.get(DcMotor.class, "rearRightDrive"); //ch0
+        elevatorDrive = hardwareMap.get(DcMotor.class, "elevatorDrive"); //eh3
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
@@ -84,9 +86,13 @@ public class BasicOpMode_Linear extends LinearOpMode {
         frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
         rearLeftDrive.setDirection(DcMotor.Direction.FORWARD);
         rearRightDrive.setDirection(DcMotor.Direction.REVERSE);
-        elevatorDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        elevatorDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        elevatorDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        elevatorDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Wait for the game to start (driver presses PLAY)
+        elevatorDrive.setTargetPosition(0);
         waitForStart();
         runtime.reset();
 
@@ -98,18 +104,19 @@ public class BasicOpMode_Linear extends LinearOpMode {
             double frontRightPower;
             double rearLeftPower;
             double rearRightPower;
-            double elevatorPower = gamepad1.left_trigger - gamepad1. right_trigger;
+            double totalDistance;
+            double elevatorPower = gamepad1.left_trigger - gamepad1.right_trigger;
 
             // POV Mode uses left stick to go forward and strafe, and right stick to turn.
             // - This uses basic math to combine motions and is easier to drive straight.
             double drive = gamepad1.left_stick_y;
             double strafe = gamepad1.left_stick_x;
-            double turn  = -gamepad1.right_stick_x;
-            frontLeftPower    = Range.clip(drive + turn - strafe, -1.0, 1.0);
-            rearLeftPower    = Range.clip(drive + turn + strafe, -1.0, 1.0);
-            frontRightPower   = Range.clip(drive - turn + strafe, -1.0, 1.0);
-            rearRightPower   = Range.clip(drive - turn - strafe, -1.0, 1.0);
-            elevatorPower  = Range.clip(elevatorPower, -0.1, 0.1);
+            double turn = -gamepad1.right_stick_x;
+            frontLeftPower = Range.clip(drive + turn - strafe, -1.0, 1.0);
+            rearLeftPower = Range.clip(drive + turn + strafe, -1.0, 1.0);
+            frontRightPower = Range.clip(drive - turn + strafe, -1.0, 1.0);
+            rearRightPower = Range.clip(drive - turn - strafe, -1.0, 1.0);
+            elevatorPower = Range.clip(elevatorPower, -1.0, 1.5);
 
             // Send calculated power to wheels
             frontLeftDrive.setPower(frontLeftPower);
@@ -118,16 +125,72 @@ public class BasicOpMode_Linear extends LinearOpMode {
             rearRightDrive.setPower(rearRightPower);
             elevatorDrive.setPower(elevatorPower);
 
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Front Motors", "left (%.2f), right (%.2f)", frontLeftPower, frontRightPower);
-            telemetry.addData("Rear Motors", "left (%.2f), right (%.2f)", rearLeftPower, rearRightPower);
-            telemetry.addData("Left Stick", "x (%.2f), y (%.2f)", gamepad1.left_stick_x, gamepad1.left_stick_y);
-            telemetry.addData("Right Stick", "x (%.2f), y (%.2f)", gamepad1.right_stick_x, gamepad1.right_stick_y);
-            telemetry.addData("D-PAD", "l (%b), r (%b)", gamepad1.dpad_left, gamepad1.dpad_right);
-            telemetry.addData("elevatorPower","(power %.2f)", elevatorPower);
-            telemetry.update();
+            totalCounts = elevatorDrive.getCurrentPosition();
+            totalDistance = totalCounts / MOTOR_PPR;
+
+            if (gamepad1.a) {
+                elevatorDrive.setPower(1);
+                while (totalCounts < 0) {
+                    totalCounts = elevatorDrive.getCurrentPosition();
+                    if (gamepad1.start) {
+                        elevatorDrive.setPower(0);
+                        break;
+                    }
+                }
+                elevatorDrive.setPower(0);
+
+            } else if (gamepad1.b) {
+                elevatorDrive.setPower(-1);
+                while (totalCounts > -3941) {
+                    totalCounts = elevatorDrive.getCurrentPosition();
+                    if (gamepad1.start) {
+                        elevatorDrive.setPower(0);
+                        break;
+                    }
+                }
+                elevatorDrive.setPower(0);
+
+            } else if (gamepad1.x) {
+                elevatorDrive.setPower(-1);
+                while (totalCounts > -6647) {
+                    totalCounts = elevatorDrive.getCurrentPosition();
+                    if (gamepad1.start) {
+                        elevatorDrive.setPower(0);
+                        break;
+                    }
+                }
+                elevatorDrive.setPower(0);
+
+            } else if (gamepad1.y) {
+                elevatorDrive.setPower(-1);
+                while (totalCounts > -10000) {
+                    totalCounts = elevatorDrive.getCurrentPosition();
+                    if (gamepad1.start) {
+                        elevatorDrive.setPower(0);
+                        break;
+                    }
+                }
+                elevatorDrive.setPower(0);
+
+                // Show the elapsed game time and wheel power.
+                telemetry.addData("Status", "Run Time: " + runtime.toString());
+                telemetry.addData("Front Motors", "left (%.2f), right (%.2f)", frontLeftPower, frontRightPower);
+                telemetry.addData("Rear Motors", "left (%.2f), right (%.2f)", rearLeftPower, rearRightPower);
+                telemetry.addData("Left Stick", "x (%.2f), y (%.2f)", gamepad1.left_stick_x, gamepad1.left_stick_y);
+                telemetry.addData("Right Stick", "x (%.2f), y (%.2f)", gamepad1.right_stick_x, gamepad1.right_stick_y);
+                telemetry.addData("D-PAD", "l (%b), r (%b)", gamepad1.dpad_left, gamepad1.dpad_right);
+                telemetry.addData("elevatorPower", "(power %.2f)", elevatorPower);
+                telemetry.addData("Encoder Count", "(%7d)", totalCounts);
+                telemetry.addData("Num Rotations", "(%.2f)", totalDistance);
+                telemetry.update();
+            }
         }
     }
 }
+
+//Start
+//B button: -3941
+//X button: -6647
+//Y button: -10000
+
 
