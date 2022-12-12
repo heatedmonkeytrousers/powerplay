8e1113aebb6a6529a9bfe258f1b61062e1dc49c3
Mizukiee <codingforfun156@gmail.com>
2022-12-11 18:54:32 -0600

    Claw enum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
@@ -4,12 +4,18 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Claw extends  Thread {
-    static final double INCREMENT   = 0.001;
     static public double START_MAX_POS = 0.26;
     static final double MAX_POS     =  0.24;
     static final double MIN_POS     =  0.1;
     private Servo clawServo;
     private Gamepad gamepad;
+
+    public enum CLAW_POSITION
+    {
+        START,
+        OPEN,
+        CLOSE
+    }
 
 
     public Claw(Servo clawServo, Gamepad gamepad) {
@@ -17,6 +23,21 @@ public class Claw extends  Thread {
         this.gamepad = gamepad;
     }
 
+    public void setClawPosition(CLAW_POSITION clawPosition) {
+        switch(clawPosition) {
+            case START:
+                clawServo.setPosition(START_MAX_POS);
+                break;
+            case OPEN:
+                clawServo.setPosition(MAX_POS);
+                break;
+            case CLOSE:
+                clawServo.setPosition(MIN_POS);
+                break;
+            default:
+                return;
+        }
+    }
     public void closeClaw () {
         clawServo.setPosition(MIN_POS);
     }
